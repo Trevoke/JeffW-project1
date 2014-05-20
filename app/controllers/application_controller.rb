ee82353ea229f73fb92c5bbcb46ed3d6b9a1b26b
jwink <jeff_winkler@yahoo.com>
2014-05-20 11:16:42 -0400

    fine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ end
 #        investors_new GET    /investors/new(.:format)                            investors#new check, ok open to all
 #            investors POST   /investors(.:format)                                investors#create check, ok not seen
 #                      GET    /portfolios/analyze/:id(.:format)                   portfolios#analyze check, need to check security
-#                      GET    /stocks/:sym(.:format)                              stocks#display check, need to check security
+#                      GET    /stocks/:sym(.:format)                              stocks#display check, ok open to all
 #                      GET    /portfolios/:portfolio_id/stocks/exists(.:format)   stocks#exists check, self protecting
 #                      POST   /portfolios/:portfolio_id/stocks(.:format)          stocks#create check, self protecting
 #  new_portfolio_stock GET    /portfolios/:portfolio_id/stocks/new(.:format)      stocks#new check, ok put auth in
--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-
-Investor.delete_all
-Day.delete_all
-Stock.delete_all
-Share.delete_all
-Portfolio.delete_all
