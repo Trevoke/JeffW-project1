3512a1745a5539e3a748550dc8e0c8d623f29f78
jwink <jeff_winkler@yahoo.com>
2014-05-20 11:05:47 -0400

    clean tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,6 @@
 
-
-#i1 = Investor.create(username: 'user1', actualname: 'Lichard Grey')
-# p1 = Portfolio.create(name: 'Tech')
-
-# i1.portfolios << p1
-
-# s1 = Stock.create(ticker: 'LO', name: 'Lorillard')
-
-
-# t1 = Time.new(2012, 5, 1)
-# t2 = Time.new(2014, 5, 14)
-
-
-# data = YahooFinance.historical_quotes("#{s1.ticker}", t1, t2,{ raw: false, period: :daily })
-
-# count = data.count - 1
-
-
-# for x in (count).downto(0)
-
-#   d1 = Day.create(trade_date: data[x].trade_date, symbol: data[x].symbol, close: data[x].close)
-
-#   s1.days << d1
-
-# end
-
-
+Investor.delete_all
+Day.delete_all
+Stock.delete_all
+Share.delete_all
+Portfolio.delete_all
